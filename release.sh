f834735beabe082dfae07540fe29b0a68fed2d70
Ryota Arai <ryota.arai@gmail.com>
2014-11-26 15:27:44 +0900

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+if [ -z "$1" ]; then
+    echo "Usage: release.sh VERSION"
+    exit 1
+fi
+
 set -ex
 
 #gox -os="darwin linux windows" -arch="386 amd64" -output="pkg/{{.Dir}}_{{.OS}}_{{.Arch}}"
 gox -os="linux" -arch="386 amd64" -output="pkg/{{.Dir}}_{{.OS}}_{{.Arch}}"
+git tag v$1
+git push --tags
 ghr -u ryotarai $1 pkg/
