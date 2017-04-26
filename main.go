cb3f8483f6bc98c3f86f297805f9da1993cd0fe9
Ryota Arai <ryota.arai@gmail.com>
2017-04-26 16:32:23 +0900

    Cache tags of EC2 instance if -cache-ttl is set.

--- main.go
+++ main.go
@@ -4,29 +4,74 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
-	log "github.com/Sirupsen/logrus"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"io/ioutil"
-	"time"
 	"net/http"
 	"os"
 	"path"
 	"regexp"
 	"strings"
 	"text/template"
+	"time"
+
+	"encoding/json"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 var version = "0.1.5"
+var tagsCacheFilePrefix = "/tmp/confit-instance-tags-cache"
 
 type EC2Helper struct {
-	client *ec2.EC2
+	client   *ec2.EC2
+	cacheTTL time.Duration
 }
 
-func (h *EC2Helper) getInstanceById(instanceId string) (*ec2.Instance, error) {
+func (h *EC2Helper) getInstanceTags(instanceId string) (map[string]string, error) {
+	file := fmt.Sprintf("%s.%s", tagsCacheFilePrefix, instanceId)
+	info, err := os.Stat(file)
+	if err == nil {
+		d := time.Now().Sub(info.ModTime())
+		if d <= h.cacheTTL {
+			log.Info("Using cache")
+			j, err := ioutil.ReadFile(file)
+			if err != nil {
+				return nil, err
+			}
+
+			tags := map[string]string{}
+			err = json.Unmarshal(j, &tags)
+			if err != nil {
+				return nil, err
+			}
+			return tags, nil
+		}
+	}
+
+	tags, err := h.getInstanceTagsWithoutCache(instanceId)
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := json.Marshal(tags)
+	if err != nil {
+		return nil, err
+	}
+
+	err = ioutil.WriteFile(file, b, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	return tags, nil
+}
+
+func (h *EC2Helper) getInstanceTagsWithoutCache(instanceId string) (map[string]string, error) {
 	params := &ec2.DescribeInstancesInput{
-		InstanceIDs: []*string{
+		InstanceIds: []*string{
 			aws.String(instanceId),
 		},
 	}
@@ -39,7 +84,11 @@ func (h *EC2Helper) getInstanceById(instanceId string) (*ec2.Instance, error) {
 	reservation := resp.Reservations[0]
 	instance := reservation.Instances[0]
 
-	return instance, nil
+	tags := map[string]string{}
+	for _, t := range instance.Tags {
+		tags[*t.Key] = *t.Value
+	}
+	return tags, nil
 }
 
 type S3Helper struct {
@@ -85,6 +134,7 @@ func main() {
 	prefixFormat := flag.String("prefix", "", "key prefix")
 	createDirectory := flag.Bool("create-directory", true, "create destination directory automatically?")
 	instanceId := flag.String("debug-instance-id", "", "instance id (for debug)")
+	cacheTTLStr := flag.String("cache-ttl", "0s", "TTL for instance tags cache")
 	debug := flag.Bool("debug", false, "debug mode")
 
 	flag.Parse()
@@ -93,28 +143,36 @@ func main() {
 		log.SetLevel(log.DebugLevel)
 	}
 
+	cacheTTL, err := time.ParseDuration(*cacheTTLStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	log.Infof("Starting Confit v%v", version)
 	log.Debugf("Bucket: %v", *bucketName)
 	log.Debugf("Prefix format: %v", *prefixFormat)
 	log.Debugf("Create destination directory automatically?: %v", *createDirectory)
 
-	var awsLogLevel uint
+	var awsLogLevel aws.LogLevelType
 	if *debug {
-		awsLogLevel = 1
+		awsLogLevel = aws.LogDebug
 	} else {
-		awsLogLevel = 0
+		awsLogLevel = aws.LogOff
 	}
 
 	awsConfig := aws.Config{
-		LogLevel: awsLogLevel,
+		LogLevel: &awsLogLevel,
 	}
 
-	ec2 := ec2.New(&awsConfig)
+	sess := session.Must(session.NewSession(&awsConfig))
+
+	ec2 := ec2.New(sess)
 	ec2Helper := EC2Helper{
-		client: ec2,
+		client:   ec2,
+		cacheTTL: cacheTTL,
 	}
 
-	s3 := s3.New(&awsConfig)
+	s3 := s3.New(sess)
 	s3Helper := S3Helper{
 		client: s3,
 	}
@@ -137,14 +195,9 @@ func main() {
 	}
 	log.Debugf("Instance ID is %v", *instanceId)
 
-	instance, err := ec2Helper.getInstanceById(*instanceId)
+	tags, err := ec2Helper.getInstanceTags(*instanceId)
 	if err != nil {
 		log.Fatal(err)
-	}
-
-	tags := map[string]string{}
-	for _, tag := range instance.Tags {
-		tags[*tag.Key] = *tag.Value
 	}
 
 	// render prefix
